1afaf956e7e845dfd10dcb53c94417be841d3b6c
clgsg <clg_listas@yahoo.es>
2022-02-06 15:44:52 +0100

    fix: correct endpoint spelling

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 
+const getUsers = require('./getUsers')
 const deleteAccount = require("./deleteAccount");
-const login = require("./login");
-const signup = require("./signup");
-const updateUser = require("./updateUser");
+// const login = require("./login");
+// const signup = require("./signup");
+// const updateUser = require("./updateUser");
 
+router.get('/', getUsers);
 router.get('/delete', deleteAccount);
-router.get("/login", login);
-router.get("/signup", signup);
-router.get("/update", updateUser);
+// router.get("/login", login);
+// router.get("/signup", signup);
+// router.get("/update", updateUser);
 
 module.exports=router;
